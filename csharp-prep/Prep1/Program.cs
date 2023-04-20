26fa8cbb8dd9af87867ec56ea014309aa94deb13
Tanner Elliott <Lynnte@byui.edu>
2023-04-20 14:36:59 -0600

    cs prep 1 and 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
+// class Name
+// {
+//     static void Second(string[] args)
+//     {
+//         Console.Write("What is your first name? ");
+//         string firstName = Console.ReadLine();
+
+//         Console.Write("What is your last name? ");
+//         string lastName = Console.ReadLine();
+
+//         Console.Write($"Your name is {lastName}, {firstName} {lastName}.");
+//     }
+// }
