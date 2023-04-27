07fa9b798503000b0ede529d03ca88841d02d08f
Tanner Elliott <Lynnte@byui.edu>
2023-04-27 14:58:12 -0600

    prep 3 and 4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random generator = new Random();
+        int magicNumber = generator.Next(1, 101);
+
+        int count = 0;
+        bool again = true;
+        bool incorrect = true;
+
+        while (again == true)
+        {
+            while (incorrect == true)
+            {
+                Console.WriteLine("This is a number guessing game.");
+                Console.Write("Guess a number between 1 and 100: ");
+                string userGuess = Console.ReadLine();
+
+                int userNumber = int.Parse(userGuess);
+                count = count + 1;
+
+                if (userNumber > magicNumber){
+                    Console.WriteLine("Too High");
+                }   
+                else if (userNumber < magicNumber){
+                    Console.WriteLine("Too Low");
+                }
+                else if (userNumber == magicNumber){
+                    Console.WriteLine("That is correct");
+                    Console.WriteLine($"It took you {count} guesses.");
+                    incorrect = false;
+                }
+                else{
+                    Console.WriteLine("Is that a number?");
+                }
+            }
+            Console.WriteLine("Do you wish to play again?");
+            string shouldContinue = Console.ReadLine().ToLower();
+
+            if (shouldContinue == "yes"){
+                count = 0;
+            }
+            else if (shouldContinue == "no"){
+                again = false;
+            }
+        }
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        var numbers = new List<int>();
+
+        while (true){
+
+            Console.Write("Please enter a number (enter 0 to stop): ");
+            string stringNumber = Console.ReadLine();
+            int number = int.Parse(stringNumber);
+
+            if (number == 0) {
+                break;
+            }
+
+            numbers.Add(number);
+        }
+        int sum = 0;
+        int count = 0;
+        foreach (var num in numbers) {
+            sum = sum + num;
+            count = count + 1;
+        }
+
+        Console.WriteLine($"The sum of the numbers in the list is {sum}");
+
+        int average = sum / count;
+
+        Console.WriteLine($"The average of the list is {average}");
+
+        // float average = (float)sum / (float)numbers.Count; better way to do it.
     }
 }
