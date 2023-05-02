f99390f1c5a7512d34f78a6596a47e6af01d4a9e
Tanner Elliott <Lynnte@byui.edu>
2023-05-02 14:58:05 -0600

    sandbox and learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        var job1 = new Job("Software Engineer");
+        var job2 = new Job("Quality Assurance Officer");
+
+
     }
+}
+
+public class Job
+{
+    string _jobTitle;
+    string _company;
+    string _startYear;
+    string _endYear;
+
+    public Job(string jt, string cm, string sy, string ey) {
+        _jobTitle = jt;
+
+        _company = cm;
+
+        _startYear = sy;
+        int sty = int.Parse(sy);
+
+        _endYear = ey;
+        int eny = int.Parse(ey);
+    }
+
+    public void Display(); {
+        Console.WriteLine($"");
+    }
+
+}
+
+public class Resume
+{
+
 }
